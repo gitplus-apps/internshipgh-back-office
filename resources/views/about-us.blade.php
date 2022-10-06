5b2cd3881622c18e07baa1f822908a14314a2a88
Nana Yeboah <yeboahnanaosei@gmail.com>
2022-10-06 22:12:58 +0000

    Add tiny change to test deployment

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
         <div class="row align-center">
             <div class="col-lg-6">
-                <h2>About Us</h2>
+                <h2>About Us.</h2>
             </div>
             <div class="col-lg-6 text-right">
                 <ul class="breadcrumb">
@@ -26,7 +26,7 @@
 <!-- End About Area -->
 <div class="about-standard-area overflow-hidden default-padding">
     <!-- Fixed Shape -->
-   
+
     <!-- End Fixed Shape -->
     <div class="container">
         <div class="row align-center">
@@ -34,13 +34,13 @@
                 <div class="thumb">
                     <img src="{{asset('assets/img/about.jpg')}}" alt="Thumb">
                     <img src="{{asset('assets/img/about4.jpg')}}" alt="Thumb">
-                   
+
                 </div>
             </div>
             <div class="col-lg-6 info">
                 <h5>About</h5>
                 <p>
-                    We created this platform as a result of the overwhelming requests by students to undertake their internship with us. We researched and found that there are so many students who are unable to secure places for their internship. Likewise, there are so many organizations seeking students to undertake some internship with them. 
+                    We created this platform as a result of the overwhelming requests by students to undertake their internship with us. We researched and found that there are so many students who are unable to secure places for their internship. Likewise, there are so many organizations seeking students to undertake some internship with them.
                 </p>
                 <p>
                     The two parties – employers and the interns – benefit mutually from this relationship. Interns get to learn on the job and have hands-on practice on the theories and models they have learnt. Employers also benefit from the contributions these interns make to alleviate them from the huge pending tasks.
@@ -49,12 +49,12 @@
                 <p>
                     And this is where Internship Ghana comes in. Our platform is dedicated to both companies and prospective interns. The companies have the opportunity to select students that meet their requirements and students also have the opportunity to intern with their desired organizations.
                 </p>
-                
+
                 <p>
                     Our processes are very simply and we continue to improve the platform and also add more services.
                 </p>
-               
-               
+
+
             </div>
         </div>
     </div>
