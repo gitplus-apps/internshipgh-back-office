c622970d9673766cffc3c02feb247cba8670ba2a
Nana Yeboah <yeboahnanaosei@gmail.com>
2022-10-11 13:22:37 +0000

    Remove unused import

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RouteController;
-use App\Http\Controllers\RegistrationController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
