4153fe1bc5a4c4a172ba0a5fd1618e82c39ef7fd
Nana Yeboah <yeboahnanaosei@gmail.com>
2022-10-27 14:43:26 +0000

    Change default logging channel to stack

--- config/logging.php
+++ config/logging.php
@@ -50,7 +50,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single', 'rollbar'],
+            'channels' => ['single'],
             'ignore_exceptions' => false,
         ],
 
