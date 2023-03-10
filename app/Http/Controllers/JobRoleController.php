a7d50376c77674b39baeb915f34a0b4f741cbc64
BernardKwesi <bernarddormon31@gmail.com>
2023-03-10 20:04:53 +0000

    Add Job categories api

--- app/Http/Controllers/JobRoleController.php
+++ app/Http/Controllers/JobRoleController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\JobRoleResource;
+use App\Models\RoleCategory;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class JobRoleController extends Controller
 {
-    //
+    public function index() : JsonResponse
+    {
+        $job_roles = RoleCategory::where('deleted','0')->get();
+        
+        return response()->json([
+            "ok"=> true,
+            "data"=> JobRoleResource::collection($job_roles)
+        ]);
+    }
 }
