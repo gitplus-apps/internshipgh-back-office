b69884e630cc5c5033066853a7e4f37c5392ce2b
BernardKwesi <bernarddormon31@gmail.com>
2023-03-10 20:15:45 +0000

    add districts api

--- app/Http/Controllers/DistrictController.php
+++ app/Http/Controllers/DistrictController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use App\Models\District;
 
 class DistrictController extends Controller
 {
-    //
+    public function index () : JsonResponse
+    {
+        $districts = District::select('tbldistrict.code','tbldistrict.name','tbldistrict.region as region_code','tblregion.description as region')
+                            ->join('tblregion','tblregion.code','tbldistrict.region')
+                            ->where('tbldistrict.deleted','0')->get();
+                            
+        return response()->json([
+            'ok'=> true,
+            "data"=> $districts
+        ]);
+    }
 }
