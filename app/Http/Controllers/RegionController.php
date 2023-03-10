be10cca715d8f848181e8d7639e4bb25db4b14a8
BernardKwesi <bernarddormon31@gmail.com>
2023-03-10 20:08:45 +0000

    add region api

--- app/Http/Controllers/RegionController.php
+++ app/Http/Controllers/RegionController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Region;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class RegionController extends Controller
 {
-    //
+    public function index() :JsonResponse
+    {
+        
+        $regions = Region::select('transid','code', 'description')->where('deleted','0')->get();
+        
+        return response()->json([
+                "ok"=> true,
+                "data"=> $regions
+        ]);
+    }
 }
